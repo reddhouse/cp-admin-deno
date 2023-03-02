186502d17256309223b5a9563db0cabc5e4b37cf
JMT <reddhouse@gmail.com>
2023-03-02 14:41:39 -0800

    Change to terminal-based interface

---
+++ cp_admin_script.ts
@@ -0,0 +1,31 @@
+// Run this script with...
+// deno run --allow-env --allow-read --allow-run cp_admin_script.ts
+
+interface Command {
+  label: string;
+  commands: string[];
+}
+
+const menu: { [key: string]: Command } = {
+  "0": { label: "Exit", commands: [] },
+  "1": { label: "Terraform Init", commands: ["terraform", "init"] },
+};
+
+const handleAction = async (selection: string) => {
+  const p = Deno.run({ cmd: menu[selection].commands });
+  const { code } = await p.status();
+  console.log(`Process ${p.pid} exited with code ${code}.`);
+};
+
+while (true) {
+  // Display menu.
+  for (const [key, value] of Object.entries(menu)) {
+    console.log(`${key}: ${value.label}`);
+  }
+  const selection = prompt("Select an action:", "0");
+  if (selection == "0") {
+    break;
+  }
+  // Do stuff based on selection.
+  await handleAction(selection ?? "0");
+}

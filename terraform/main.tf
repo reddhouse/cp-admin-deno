36eb1a8f28b4b60fc31d9b7a1968225c37e72bfc
JMT <reddhouse@gmail.com>
2023-03-04 06:17:42 -0800

    Clean up terraform + use templatefile for cloud config

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,37 +4,8 @@ variable "hcloud_token" {
   # Leaving blank here, and will provide as command line argument.
 }
 
-variable "location" {
-  default = "hil"
-}
 
-variable "http_protocol" {
-  default = "http"
-}
-
-variable "http_port" {
-  default = "80"
-}
-
-variable "instances" {
-  default = "1"
-}
-
-variable "server_type" {
-  default = "cpx11"
-}
-
-variable "os_type" {
-  # Jammy Jellyfish with standard support until 2027-04-21 
-  default = "ubuntu-20.04"
-}
-
-variable "disk_size" {
-  default = "40"
-}
-
-
-## Provider
+## Providers
 
 terraform {
   required_providers {
@@ -50,54 +21,46 @@ provider "hcloud" {
 }
 
 ## Resources
-
-resource "hcloud_server" "cp-server" {
-  count       = var.instances
-  name        = "cp-server-${count.index}"
-  image       = var.os_type
-  server_type = var.server_type
-  location    = var.location
-  ssh_keys    = [hcloud_ssh_key.jmt-mac-mini-key.id]
-  user_data   = file("user_data.yml")
-}
-
 resource "hcloud_ssh_key" "jmt-mac-mini-key" {
   name       = "jmt-mac-mini-key"
   public_key = file("~/.ssh/id_rsa.pub")
 }
 
-resource "hcloud_network" "cp-network" {
-  name     = "cp-network"
-  ip_range = "10.0.0.0/8"
+resource "hcloud_server" "coopar-server-1" {
+  name = "coopar-1"
+  # Jammy Jellyfish with standard support until 2027-04-21 
+  image       = "ubuntu-20.04"
+  server_type = "cpx11"
+  location    = "hil"
+  ssh_keys    = [hcloud_ssh_key.jmt-mac-mini-key.id]
+  # Use templatefile function to insert dynamic value of public ssh key(s) into cloud config file.
+  user_data = templatefile("${path.module}/user_data.yml", { jmt-mac-mini-key = file("~/.ssh/id_rsa.pub") })
 }
 
-resource "hcloud_network_subnet" "cp-subnet" {
-  network_id   = hcloud_network.cp-network.id
+resource "hcloud_network" "coopar-vpc" {
+  name     = "coopar-vpc"
+  ip_range = "10.0.0.0/16"
+}
+
+resource "hcloud_network_subnet" "coopar-public-subnet" {
+  network_id   = hcloud_network.coopar-vpc.id
   type         = "cloud"
   network_zone = "us-west"
-  ip_range     = "10.0.1.0/24"
+  ip_range     = "10.0.0.0/24"
 }
 
-resource "hcloud_server_network" "cp-server-network" {
-  count      = var.instances
-  server_id  = hcloud_server.cp-server[count.index].id
-  network_id = hcloud_network.cp-network.id
-  ip         = "10.0.1.5"
+resource "hcloud_server_network" "cp-server-1-network" {
+  server_id = hcloud_server.coopar-server-1.id
+  subnet_id = hcloud_network_subnet.coopar-public-subnet.id
 }
 
 
 ## Outputs
 
-output "app_servers_status" {
-  value = {
-    for server in hcloud_server.cp-server :
-    server.name => server.status
-  }
+output "coopar-server-1-status" {
+  value = hcloud_server.coopar-server-1.status
 }
 
-output "app_servers_ips" {
-  value = {
-    for server in hcloud_server.cp-server :
-    server.name => server.ipv4_address
-  }
+output "coopar-server-1-ip" {
+  value = hcloud_server.coopar-server-1.ipv4_address
 }
